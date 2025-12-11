f0ee977cfbb906d7c1cb33bbb45cf945a83b666b
kaenkai <karol.kulinowski@gmail.com>
2025-12-12 00:49:13 +0100

    AoC2025 day 6 part 2 simplified

--- AoC2025/Day6/main.py
+++ AoC2025/Day6/main.py
@@ -20,30 +20,23 @@ def part2(in_file: str) -> None:
     grand_total = 0
     with open(in_file, 'r') as file:
         worksheet = file.readlines()
-        m = len(worksheet[0])-1  # -1 ommits '\n'
-        n = len(worksheet)-1  # -1 ommits operations
-        nums = ['']*m
-        for i in range(len(worksheet)-1):
-            r = worksheet[i][:-1]
-            for j, s in enumerate(r):
-                nums[j] += s
-        nums = [n.strip() for n in nums if n.strip()]
         operations = worksheet[-1].strip().split()
-        eq_len = [0]
-        for s in worksheet[-1][1:]:
-            if not s.strip(): eq_len[-1] += 1
-            else: eq_len.append(0)
-        eq_len[-1] += 1
-        offset = 0
+        cols = ['']*(len(worksheet[0])-1)
+        for i in range(len(worksheet)-1):
+            for j, s in enumerate(worksheet[i][:-1]):
+                cols[j] += s
+        eqns = [[]]
+        for eq in [c.strip() for c in cols]:
+            if eq: eqns[-1].append(eq)
+            else: eqns.append([])
         for i, o in enumerate(operations):
             if o == '+':
                 res = 0
-                for num in nums[offset:offset+eq_len[i]]: res += int(num)
+                for num in eqns[i]: res += int(num)
             elif o == '*':
                 res = 1
-                for num in nums[offset:offset+eq_len[i]]: res *= int(num)
+                for num in eqns[i]: res *= int(num)
             grand_total += res
-            offset += eq_len[i]
     print('Grand total:', grand_total)
 
 
